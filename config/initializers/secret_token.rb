accf4bad4eadf10ac9b9faeda6a4be1c4844793d
Arturs Kreipans <arturs.kreipans@gmail.com>
2012-08-11 14:42:19 +0100

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stinkytrainers::Application.config.secret_token = '5cd72f08c69d594fe862c25e375d965c0d5abb20fe759042b59d8c8d1e6e9c2993a01ab3bb218b481f578f88da53922e697f3f74cc709042deb2449830e911a5'
