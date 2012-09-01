fa8b5e6b0baafde9580b8e7efb8797e456f8e617
Your Name <stephen.creedon@rapidappsgroup.com>
2012-09-01 14:04:43 +0100

    ??

--- doc/h2-named-anchors.rb
+++ doc/h2-named-anchors.rb
@@ -1,1 +1,17 @@
-File.open
+dir = File.dirname(__FILE__)
+
+Dir.glob("#{dir}/*.mdown").each do |path|
+  file = File.open(path)
+  puts path.gsub("./","processed/")
+  processed_file = File.open(path.gsub("./","processed/"), "w") do |pf|
+    
+    file.readlines.each do |line|
+     pf.write line if line =~ /^##[^#]/
+    end
+  
+  end
+  
+  
+  
+  
+end
