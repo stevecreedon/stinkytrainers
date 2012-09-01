e275e503719c5343d624b21d8f8a0fbcd7a99868
Your Name <stephen.creedon@rapidappsgroup.com>
2012-09-01 14:44:03 +0100

    added processing to create indexes from ## (h2)

--- doc/h2-named-anchors.rb
+++ doc/h2-named-anchors.rb
@@ -1,14 +1,38 @@
+class String
+  def canonicalize
+    self.downcase.gsub(/[^a-z0-9]/,"-").squeeze("-").gsub(/^-/,"").gsub(/-$/,"")
+  end
+end
+
+
 dir = File.dirname(__FILE__)
 
 Dir.glob("#{dir}/*.mdown").each do |path|
-  file = File.open(path)
+  file = File.open(path, "r")
   puts path.gsub("./","processed/")
   processed_file = File.open(path.gsub("./","processed/"), "w") do |pf|
     
+    h2s = []
+    
     file.readlines.each do |line|
-     pf.write line if line =~ /^##[^#]/
+        if line =~ /^##[^#]/
+          h2s << [line.gsub(/^##[^a-zA-Z]+/,"").gsub("\n",""), line.canonicalize]
+        end
     end
-  
+    
+    file.rewind
+    
+    file.readlines.each do |line|
+      puts line
+        if line =~ /^INDEX/
+          h2s.each do |h2|
+            pf.write "1. [#{h2[0]}](##{h2[1]})\n"
+          end
+        else
+          pf.write line
+        end
+    end
+    
   end
   
   
--- lib/tasks/markdown.rake
+++ lib/tasks/markdown.rake
@@ -5,9 +5,9 @@ namespace :tutorial do
 
     markdown = Redcarpet::Markdown.new( PygmentizeHTML.new, options )
 
-    Dir.glob("{doc}/**/*.mdown").each do |file_name|
+    Dir.glob("{doc}/processed/**/*.mdown").each do |file_name|
       # change fail path from doc/some-file.mdown to app/views/tutuorial/some-file.html
-      target = File.join('app/views/tutorial/', "#{file_name.gsub(/^doc\//, '').gsub(/\.mdown$/,'')}.html")
+      target = File.join('app/views/tutorial/', "#{file_name.gsub(/^doc\/processed\//, '').gsub(/\.mdown$/,'')}.html")
 
       puts "Converting #{file_name} to #{target}"
 
